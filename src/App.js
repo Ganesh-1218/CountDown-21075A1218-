d3d6f4ef7bb744371b5967fb3a8f9b5ede923df4
Ganesh <sangamganesh5049@gmail.com>
2023-12-20 22:51:15 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import HomeFile from './components/HomeFile'
 import './components/Display.css'
 const App = () => {
   return (
-    <div className='TOTAL-BODY'>
-        <HomeFile/>
+    <div>
+        <div className='background-img'>
+          <img src='https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_1280.jpg' width="100%"/>
+        </div>
+       <div className='content'>
+       <HomeFile/> 
+       </div>
     </div>
   )
 }
